e171dc5ebe410fb2d5c3f9b25b251c147c603ff9
Byron <byronblocker@gmail.com>
2017-04-25 14:26:27 -0700

    add method for estimating total

--- GroceryList.java
+++ GroceryList.java
@@ -1,14 +1,22 @@
 public class GroceryList {
   public static void main(String[] args) {
     String[] myGroceryList = {"ice cream", "hot sauce", "pickles", "bananas", "cereal", "rice", "yogurt"};
-    String[] heroTeam = {"The Flash", "Superman", "Wonder Woman"};
+    //String[] heroTeam = {"The Flash", "Superman", "Wonder Woman"};
+    Integer[] groceryItemPrices = {3,6,4,2,4,4};
 
     for (String groceryItem : myGroceryList) {
       System.out.println(groceryItem);
     }
 
-    for (String hero : heroTeam) {
-      System.out.println(hero);
+    Integer total = 0;
+    for (Integer price : groceryItemPrices) {
+      total += price;
     }
+
+    System.out.println("Your total will be $" + total);
+
+    // for (String hero : heroTeam) {
+    //   System.out.println(hero);
+    // }
   }
 }
