71d9e3b094820ea6afca43219d1c54469132ef81
Byron <byronblocker@gmail.com>
2017-04-25 14:32:51 -0700

    program to index through a grocery list

---
+++ IndexList.java
@@ -0,0 +1,9 @@
+public class IndexList {
+  public static void main(String[] args) {
+    String[] groceryList = {"egges", "milk", "bread", "bananas", "cereal", "rice"};
+
+    for (Integer index = 0; index < groceryList.length; index++) {
+      System.out.println(groceryList[index]);
+    }
+  }
+}
