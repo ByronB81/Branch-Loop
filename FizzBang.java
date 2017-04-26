f9ffd0122e160e6051b6c77001158270babe3a76
Byron Blocker <byronblocker@gmail.com>
2017-04-25 18:19:37 -0700

    Added in rest of fizz bang = works

--- FizzBang.java
+++ FizzBang.java
@@ -11,6 +11,10 @@ public class FizzBang {
     for (Integer i = 1; i < num; i++)
       if (i % 15 == 0) {
         System.out.println("F I Z Z B A N G muthafucka!!!");
+      } else if (i % 5 == 0){
+        System.out.println("Bang bang --- He shot me down");
+      } else if (i % 3 == 0) {
+        System.out.println("Fizzzzzzzzz");
       } else {
         System.out.println(i);
       }
