ff3d3e61053a9b144bdde4748bd094b0d5cf11cc
Byron <byronblocker@gmail.com>
2017-04-27 10:43:01 -0700

    program that outputs only part of an array

---
+++ EatFull.java
@@ -0,0 +1,12 @@
+public class EatFull {
+  public static void main(String[] args) {
+    String[] foods = {"pizza","peanut butter balls","ramen", "bbq","peas","meatlaof", "M&M's","tacos","jalepenos","ice cream"};
+    Integer full = 5;
+
+    for (int i=0; i<full; i++) {
+    System.out.println(foods[i]);
+    }
+    System.out.println("Oh please stop... I'm FULLLLLLLL");
+
+  }
+}
