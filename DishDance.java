d4eb8cb5c48a80f7e9e5030f07c124c9959ceae8
Byron <byronblocker@gmail.com>
2017-04-27 11:10:53 -0700

    add in fun wording

--- DishDance.java
+++ DishDance.java
@@ -7,10 +7,10 @@ public class DishDance {
     System.out.println("How many dishes would you like for dinner?");
     String strNum = console.readLine();
     Integer num = Integer.parseInt(strNum);
-    System.out.println(num + "! My, my, someone is hungry. Coming right up!");
+    System.out.println(num + " dishes?! My, my, someone is hungry. Coming right up!");
      for (Integer i = 0; i < num; i++) {
        System.out.println("let's try some " + dishes[i]);
      }
-
+     System.out.println("Okay that's all " + num + " dishes. I hope you're full!");
   }
 }
